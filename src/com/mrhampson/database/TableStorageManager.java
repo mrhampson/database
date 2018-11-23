15054558de61d8c6bab73b171503344a4d75db86
Marshall Hampson <mhampson@liveaction.com>
2018-11-23 00:43:19 -0800

    Remove file header

--- src/com/mrhampson/database/TableStorageManager.java
+++ src/com/mrhampson/database/TableStorageManager.java
@@ -1,16 +1,3 @@
-/*
- * TableStorageManager.java
- * Created on Nov 10, 2018, 6:45 PM
- *
- * Copyright 2008-2018 LiveAction, Incorporated. All Rights Reserved.
- * 3500 W Bayshore Road, Palo Alto, California 94303, U.S.A.
- *
- * This software is the confidential and proprietary information
- * of LiveAction ("Confidential Information").
- * You shall not disclose such Confidential Information and shall use
- * it only in accordance with the terms of the license agreement
- * you entered into with LiveAction.
- */
 package com.mrhampson.database;
 
 import java.io.IOException;
--- src/com/mrhampson/database/VarCharColumnValue.java
+++ src/com/mrhampson/database/VarCharColumnValue.java
@@ -1,16 +1,3 @@
-/*
- * VarCharColumnValue.java
- * Created on Nov 11, 2018, 12:06 AM
- *
- * Copyright 2008-2018 LiveAction, Incorporated. All Rights Reserved.
- * 3500 W Bayshore Road, Palo Alto, California 94303, U.S.A.
- *
- * This software is the confidential and proprietary information
- * of LiveAction ("Confidential Information").
- * You shall not disclose such Confidential Information and shall use
- * it only in accordance with the terms of the license agreement
- * you entered into with LiveAction.
- */
 package com.mrhampson.database;
 
 import java.nio.ByteBuffer;
